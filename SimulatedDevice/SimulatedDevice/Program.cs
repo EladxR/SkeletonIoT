dcc81670d32e16297901242988e921415024c45c
GalUziel <73026724+GalUziel@users.noreply.github.com>
2022-04-11 11:31:23 +0300

    simulated device

---
+++ SimulatedDevice/SimulatedDevice/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+
+namespace SimulatedDevice
+{
+    class Program
+    {
+        private const int TakeSpot = 1;
+        private const int LeaveSpot = 2;
+
+        private const string DeviceConnectionString = @"HostName=SkeletonHub.azure-devices.net;DeviceId=ourdevice;SharedAccessKey=i8qhh8TWILiUJ5abCP7FqwOK0+RBgQEPcHGo6QnRgRc=";
+
+        private static readonly DeviceClient Client = DeviceClient.CreateFromConnectionString(DeviceConnectionString);
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Please select action number: ");
+            Console.WriteLine($"({TakeSpot}) Take Spot");
+            Console.WriteLine($"({LeaveSpot}) Leave Spot");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            while (true)
+            {
+                int action = GetSimulatedAction();
+
+                var data = new { Action = action, ActionType = "SpotStateChange" };
+                string messageJson = JsonConvert.SerializeObject(data);
+                Message message = new Message(Encoding.ASCII.GetBytes(messageJson)) { ContentType = "application/json", ContentEncoding = "utf-8"};
+
+                Console.WriteLine($"Simulating action ({action}).");
+                await Client.SendEventAsync(message);
+            }
+        }
+
+        private static int GetSimulatedAction()
+        {
+            while (true)
+            { 
+                char keyChar = Console.ReadKey(true).KeyChar;
+                if (char.IsNumber(keyChar))
+                {
+                    int number = (int)char.GetNumericValue(keyChar);
+                    if (number == TakeSpot || number == LeaveSpot)
+                        return number;
+                }
+            }
+        }
+
+        
+    }
+}
