3a6fe02f2965f4ac8fb81f5d4fcb891e1764ea60
Yoav Marco <yoavm448@gmail.com>
2022-04-21 11:08:00 +0300

    Add arduino code

---
+++ arduino-code/iot_configs.h
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+// Wifi
+#define IOT_CONFIG_WIFI_SSID "marco"
+#define IOT_CONFIG_WIFI_PASSWORD "098910177"
+// HostName=SkeletonHub.azure-devices.net;DeviceId=ourdevice;SharedAccessKey=i8qhh8TWILiUJ5abCP7FqwOK0+RBgQEPcHGo6QnRgRc=
+// Azure IoT
+#define IOT_CONFIG_IOTHUB_FQDN "SkeletonHub.azure-devices.net"
+#define IOT_CONFIG_DEVICE_ID "ourdevice"
+#define IOT_CONFIG_DEVICE_KEY "i8qhh8TWILiUJ5abCP7FqwOK0+RBgQEPcHGo6QnRgRc="
+
+// Publish 1 message every 2 seconds
+#define TELEMETRY_FREQUENCY_MILLISECS 2000
